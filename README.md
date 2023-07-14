b76c8a7356b76f92d30b365206bacda478c1f02f
Shubham Kamboj <shubham.kamboj@shorthills.ai>
2023-07-14 11:26:33 +0530

    Add setup steps for Azure openai api

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # azure_openai_setup
+
+## Setup steps for azure_openai
+1. Create and activate a virtual environment.
+2. Run `pip install -r requirements.txt`.
+3. Add `.env` file and update it with help of `env.sample` and the credentials you have.
+4. Run `python azure_openai.py` to test the connection.
---
+++ azure_openai.py
@@ -0,0 +1,22 @@
+import os
+import requests
+import json
+import openai
+
+from dotenv import load_dotenv
+load_dotenv()
+
+openai.api_key = os.getenv("AZURE_OPENAI_KEY")
+openai.api_base = os.getenv("AZURE_OPENAI_ENDPOINT") # https://YOUR_RESOURCE_NAME.openai.azure.com/
+openai.api_type = os.getenv('OPENAI_API_TYPE')
+openai.api_version = os.getenv('OPENAI_API_VERSION')
+
+deployment_name= os.getenv('DEPLOYMENT_NAME') #This will correspond to the custom name you chose for your deployment when you deployed a model. 
+
+# Send a completion call to generate an answer
+
+prompt = 'Write a tagline for an ice cream shop. '
+print(f"Prompt : {prompt}")
+response = openai.Completion.create(engine=deployment_name, prompt=prompt, max_tokens=10)
+text = response['choices'][0]['text']
+print(f"output: {text}")
